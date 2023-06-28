67c3bf6cdf2850abb46995ef2b0c04bb7b8445f4
YoshiOlivera2023 <yo.olivera@duocuc.cl>
2023-06-27 23:53:59 -0400

    Modificación en carrito

--- carrito/urls.py
+++ carrito/urls.py
@@ -3,6 +3,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("index", views.index, name = "index"),
     path("carrito_compras", views.carrito_compras, name = "carrito_compras"),
+    path("login", views.login, name = "login"),
+    path("registro", views.registro, name = "registro"),
+    path("index", views.index, name = "index"),
+    path("nosotros", views.nosotros, name = "nosotros"),
+    path("productos", views.productos, name = "productos"),
+    path("seguimiento", views.seguimiento, name = "seguimiento"),
+    path("ayuda", views.ayuda, name = "ayuda"),
 ]
+
+
+
+
--- carrito/views.py
+++ carrito/views.py
@@ -12,3 +12,31 @@ def carrito_compras(request):
     carrito = ProductoCarrito.objects.select_related('bicicleta').all()
     context = {"productos": carrito}
     return render(request, "carrito/carrito_compras.html", context)
+
+def index(request):
+    context = {}
+    return render(request, "administrador/index.html", context)
+
+def login(request):
+    context = {}
+    return render(request, "administrador/login.html", context)
+
+def registro(request):
+    context = {}
+    return render(request, "administrador/registro.html", context)
+
+def nosotros(request):
+    context = {}
+    return render(request, "administrador/nosotros.html", context)
+
+def productos(request):
+    context = {}
+    return render(request, "administrador/productos.html", context)
+
+def seguimiento(request):
+    context = {}
+    return render(request, "administrador/seguimiento.html", context)
+
+def ayuda(request):
+    context = {}
+    return render(request, "administrador/ayuda.html", context)
